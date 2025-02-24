921356e6c597abf0b7c45bbd4ab454b17261b67e
Yauheni Prakopchyk <yauheni.prakopchyk@epicmax.co>
2025-02-24 06:48:05 +0400

    setup reveal.js project

---
+++ src/AnotherSection.vue
@@ -0,0 +1,13 @@
+<template>
+  <section>
+    <section>Vertical Slide 1</section>
+    <section>Vertical Slide 2</section>
+  </section>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const value = ref(1)
+</script>
+
---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import Reveal from 'reveal.js';
+import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
+import { onMounted } from 'vue'
+import AnotherSection from './AnotherSection.vue'
+
+onMounted(() => {
+  Reveal.initialize({
+    plugins: [Markdown]
+  });
+})
+</script>
+
+<template>
+  <div class="reveal">
+    <div class="slides">
+      <section>Single Horizontal Slide</section>
+      <section>
+        <section>Vertical Slide 1</section>
+        <section>Vertical Slide 2</section>
+      </section>
+      <AnotherSection/>
+    </div>
+  </div>
+</template>
+
+<style>
+@import url('../node_modules/reveal.js/dist/reveal.css');
+@import url('../node_modules/reveal.js/dist/theme/black.css');
+
+#app {
+  width: 100%;
+  height: 100%;
+}
+</style>
